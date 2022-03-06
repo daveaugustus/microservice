09611220686004ced2d4efa0f93cdb4d613e2436
Dave Augustus <davetweetlive@gmail.com>
2022-03-06 08:14:40 +0530

    Runnning server
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

--- authentication/main.go
+++ authentication/main.go
@@ -4,16 +4,23 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"microservice/authentication/repository"
+	"microservice/authentication/service"
 	"microservice/db"
+	"microservice/pb"
+	"net"
 
 	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
 )
 
 var (
 	local bool
+	port  int
 )
 
 func init() {
+	flag.IntVar(&port, "port", 9001, "authentication service port")
 	flag.BoolVar(&local, "local", true, "run authentication service local")
 	flag.Parse()
 }
@@ -33,5 +40,16 @@ func main() {
 	}
 	defer conn.Close()
 
-	fmt.Println("Connection: ", conn.DB().Name)
+	usersRepository := repository.NewUsersRepository(conn)
+	authService := service.NewAuthService(usersRepository)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterAuthServiceServer(grpcServer, authService)
+	log.Printf("Authentication service is running on [::]:%d\n", port)
+	grpcServer.Serve(lis)
 }
